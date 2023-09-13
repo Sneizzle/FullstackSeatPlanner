3520b70d3641d2ea37413ce3c3abfb451daeb196
John Lange <Draygoh@gmail.com>
2023-09-13 12:30:40 +0200

    Drawing Finished, ish

--- src/Components/LeafLetMapComponent.tsx
+++ src/Components/LeafLetMapComponent.tsx
@@ -1,8 +1,10 @@
-import { ImageOverlay, MapContainer, Polyline } from "react-leaflet";
+import { Marker, ImageOverlay, MapContainer, Polyline } from "react-leaflet";
 import Image from "next/image";
-import { CRS } from "leaflet";
+import { CRS, LatLng } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css"; // Re-uses images from ~leaflet package
+import "leaflet-defaulticon-compatibility";
 
 const LeafLetMapComponent = ({ coordinates }) => {
   const height: number = Math.min(
@@ -14,6 +16,40 @@ const LeafLetMapComponent = ({ coordinates }) => {
     [0, 0],
     [height, width],
   ];
+
+  const [markerPosition, setMarkerPosition] = useState<LatLng | null>(null);
+  const [trailCoordinates, setTrailCoordinates] = useState<LatLng[]>([]);
+  const [routeFinished, setRouteFinished] = useState(false);
+
+  // handle marker movement
+  useEffect(() => {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      if (currentIndex < coordinates.length) {
+        setMarkerPosition(coordinates[currentIndex]);
+        currentIndex++;
+      } else {
+        currentIndex = 0;
+        setRouteFinished(true); // Mark the route as finished
+        clearInterval(interval);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [coordinates]);
+
+  // Handle trail coords
+  useEffect(() => {
+    if (markerPosition && !routeFinished) {
+      // Only update trail while the route is not finished
+      setTrailCoordinates((prevCoordinates) => [
+        ...prevCoordinates,
+        markerPosition,
+      ]);
+    }
+  }, [markerPosition, routeFinished]);
 
   return (
     <MapContainer
@@ -28,7 +64,12 @@ const LeafLetMapComponent = ({ coordinates }) => {
     >
       <ImageOverlay bounds={bounds} url="/officepicture.png" />
 
-      <Polyline positions={coordinates} color="blue" weight={2} />
+      <Polyline positions={[]} color="blue" weight={2} />
+
+      {markerPosition && <Marker position={markerPosition}></Marker>}
+
+      {/* Create a visible trail */}
+      <Polyline positions={trailCoordinates} color="red" weight={2} />
     </MapContainer>
   );
 };
