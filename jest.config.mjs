0680c10e0ee0829b685371ce4a08e46415b185e5
John Lange <Draygoh@gmail.com>
2023-10-04 12:11:58 +0200

    hotfix

--- jest.config.mjs
+++ jest.config.mjs
@@ -13,8 +13,6 @@ const config = {
  
   testEnvironment: 'jest-environment-jsdom',
 }
-transformIgnorePatterns: [
-  "node_modules[/\\\\](?!@amcharts[/\\\\]amcharts4)"
-]
+ 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
