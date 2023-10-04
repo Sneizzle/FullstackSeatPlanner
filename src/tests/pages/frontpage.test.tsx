2dbe101c97b2612de09e321f98bf46637be0c261
John Lange <Draygoh@gmail.com>
2023-10-04 13:43:53 +0200

    helperman tested

--- src/tests/pages/frontpage.test.tsx
+++ src/tests/pages/frontpage.test.tsx
@@ -1,13 +1,39 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Home from "@/app/page";
+import Background from "@/app/Background";
+import SearchBar from "@/app/Components/SearchBar";
 
-describe("Describe frontpage", () => {
-  it("Should render properly", () => {
+// jest.mock("axios");
+describe("Homepage Testing", () => {
+  //   it("Render Title", () => {
+  //     render(<Home />);
+  //     const header = screen.getByRole("heading");
+  //     const headerText = "Who are you looking for?";
+  //     expect(header).toHaveTextContent(headerText);
+  //   });
+  it("Render Title", () => {
     render(<Home />);
+    const header = screen.getByRole("heading", {
+      name: "Who are you looking for?",
+    });
+    expect(header).toBeInTheDocument();
+  });
 
-    const header = screen.getByRole("heading");
-    const headerText = "Who are you looking for?";
-    expect(header).toHaveTextContent(headerText);
+  it("Render Background", () => {
+    render(<Background />);
+    const background = screen.getByTestId("background");
+    expect(background).toBeInTheDocument();
+  });
+
+  it("Render Searchbar", () => {
+    render(<Home />);
+    const searchbar = screen.getByPlaceholderText("Write name here"); // Assuming you have a placeholder in your SearchBar component
+    expect(searchbar).toBeInTheDocument();
+  });
+  it("Render Admin Login", () => {
+    render(<Home />);
+    const adminLoginButton = screen.getByText("Admin Login");
+    expect(adminLoginButton).toBeInTheDocument();
   });
 });
